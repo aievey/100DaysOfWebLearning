6859b15c9731218386f8454688870e5b071c8724
aievey <amitabh283verma@gmail.com>
2023-10-02 08:17:05 +0530

    o

---
+++ javaScript/script.js
@@ -0,0 +1,25 @@
+"use strict";
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreDolphins < scoreKoalas) {
+//   console.log("Koalas win the trophy");
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log("Both win the trophy");
+// }
+
+// let bill = Number(prompt("Enter the bill amount!"));
+// let tip = bill > 50 && bill < 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+
+// console.log(
+//   `Your Amount is: ${bill} and tip is ${tip}\nYou have to pay ${bill + tip}`
+// );
+/*
+  important things learned: JavaScript Fundamentals - Part1
+    type conversion and coercion
+    truthy and falsy values
+    statement vs expressions
+    the conditional ternary operator
+*/
